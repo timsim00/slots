b2cd4fcc2581a3958d33dab23042174fdd64111e
Drew Simmons <simmons.drew@gmail.com>
2019-05-02 09:19:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,107 @@
+Feedback welcome.  This is currently being used in an experimental, framework-less, build-less, micro-frontend-y app.
+
+
+# API
+
+constructor({actions, mutators, subscribers, initState, notify, namespace, debug})
+dispatch(action, payload)
+mutate(action, payload)
+subscribe(listener)
+notify(namespace)
+import({namespace, actions, initState, mutators, notify})
+
+
+# Usage
+
+// instantiate Slots somehow:
+module.default.prototype.createStore = (actions, mutators, subscribers, initialState = {}, notify, namespace, debug) => {
+  return new Slots(actions, mutators, subscribers, initialState, notify, namespace, debug)
+}
+
+## /* app-root component */
+connectedCallback() {
+  ...
+  const {actions, mutators, initState} = this
+  this.store = this.createStore({debug: true, namespace: 'app', actions, mutators, initState,
+    subscribers: [
+      this.persistState
+    ]
+  })  
+}
+
+get actions() {
+  return {
+    MY_ACTION: ({mutate, dispatch}, data) => {
+      mutate('app:SHOW_VIEW', data) // just passing through
+    }
+  }
+}
+
+get mutators() {
+  // every mutator better return state or there's gonna be trouble
+  return {
+    MY_ACTION: (state, res) => {
+      state.app.stuff = res
+      return state
+    }
+  }
+}
+
+get initState() {
+  return {
+    orgName: 'mystuff.com'
+  }
+}
+
+
+
+
+
+## /* some other module */
+
+constructor() {
+  this.app = document.querySelector('app-root')
+  this.store = this.app.store.import(this)
+
+  // if this module has a UI, subscribe to state changes:
+  this.store.subscribe((state, namespace) => this.render(state, namespace))
+}
+
+render(state, namespace) {
+  if (namespace === 'other') {
+    // UI is just a function of state
+  }
+}
+
+get namespace() {
+  return 'other'
+}
+
+get actions() {
+  return {
+    OTHER_ACTION: ({mutate}, term) => {
+      let data = {...}
+      this.app.apiRequest(data)
+      .then( res => {
+        mutate('other:OTHER_ACTION', res)
+      })
+    }
+  }
+}
+
+get mutators() {
+  return {
+    OTHER_ACTION: (state, res) => {
+      const modifiedStuff = res.filter(...)
+      state.other.otherStuff = modifiedStuff
+      return state
+    }
+  }
+}
+
+get initState() {
+  return JSON.parse(localStorage.getItem(this.namespace)) ||
+  {
+    otherStuff: []
+  }
+}
---
+++ slots.js
@@ -0,0 +1,71 @@
+class Slots { //nickname for a vending machine
+  // TODO: make reactive?, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+  constructor({actions, mutators, subscribers, initState, notify, namespace, debug}) {
+    this.actions = {}
+    this.mutators = {}
+    this.subscribers = subscribers || []
+    this.state = {}
+    this.debug = debug || false
+    this.import({actions, mutators, initState, notify, namespace})
+  }
+  dispatch(action, payload) {
+    if (this.debug) {console.log(`dispatch ${action}`, payload)}
+    if (action.includes(':')) {
+      const parts = action.split(':')
+      this.actions[parts[0]][parts[1]]({mutate: this.mutate.bind(this), state: this.state, dispatch: this.dispatch.bind(this)}, payload)
+    } else {
+      this.actions[action]({mutate: this.mutate.bind(this), state: this.state, dispatch: this.dispatch.bind(this)}, payload)
+    }
+  }
+  mutate(action, payload) {
+    if (this.debug) {console.log(`mutate ${action}`, payload)}
+    let parts = []
+    if (action.includes(':')) {
+      parts = action.split(':')
+      this.state = this.mutators[parts[0]][parts[1]](this.state, payload)
+    } else {
+      this.state = this.mutators[action](this.state, payload)
+    }
+    this.notify(parts[0])
+  }
+  subscribe(listener) {
+    // TODO: subscribe to only a namespace
+    this.subscribers.push(listener)
+    return () => {
+      this.subscribers = this.subscribers.filter(subscriber => subscriber !== listener)
+    }
+  }
+  notify(namespace) {
+    this.subscribers.forEach(subscriber => subscriber(this.state, namespace))
+  }
+  import({namespace, actions, initState, mutators, notify}) {
+    if (namespace && !this.state.hasOwnProperty(namespace)) {
+      this.state[namespace] = {}
+      this.actions[namespace] = {}
+      this.mutators[namespace] = {}
+    }
+    let _state, _actions, _mutators
+    if (namespace) {
+      _state = this.state[namespace]
+      _actions = this.actions[namespace]
+      _mutators = this.mutators[namespace]
+    } else {
+      _state = this.state
+      _actions = this.actions
+      _mutators = this.mutators
+    }
+    if (actions) {
+      _actions = Object.assign(_actions, actions)
+    }
+    if (mutators) {
+      _mutators = Object.assign(_mutators, mutators)
+    }
+    if (initState) {
+      _state = Object.assign(_state, initState)
+    }
+    if (notify) {
+      this.notify(namespace)
+    }
+    return this
+  }
+}
